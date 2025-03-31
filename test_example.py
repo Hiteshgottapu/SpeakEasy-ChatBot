8aee1262355b7678c9fd7798fced564e8007e441
Hiteshgottapu <100711330+Hiteshgottapu@users.noreply.github.com>
2025-03-31 13:40:09 +0530

    Update test_example.py

--- test_example.py
+++ test_example.py
@@ -13,4 +13,4 @@ def client():
 def test_home_route(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b'show_initialize = true' in response.data  # Ensure correct spacing
+    assert b'show_initialize = false' in response.data  # Ensure correct spacing
